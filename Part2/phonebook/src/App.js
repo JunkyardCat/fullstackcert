f04850d9cd3432c1c447b7f6dc46f778d506a8c6
unknown <JunkyardCat@users.noreply.github.com>
2023-06-03 10:51:35 +0800

    added the missing data from exercise 2b

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -37,7 +37,9 @@ const Filter = (props) =>{
 const App = () =>{
     const [persons, setPersons]= useState([
             {name:'Arto Hellas', number:'040-123456', id: 1},
-            {name:'Ada Lovelace', number:'39-44-5324532', id: 2}
+            {name:'Ada Lovelace', number:'39-44-5324532', id: 2},
+            {name: 'Dan Abramov', number: '12-43-234345', id: 3},
+            {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
     ])
     const [newName, setNewName]= useState('')
     const [newNumber, setNewNumber]= useState('')
