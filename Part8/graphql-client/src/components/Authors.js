2b8386029ac9f74778769fb437cc5a14ef37dc6d
unknown <JunkyardCat@users.noreply.github.com>
2023-07-25 00:22:22 +0800

    finished 8.11

--- Part8/graphql-client/src/components/Authors.js
+++ Part8/graphql-client/src/components/Authors.js
@@ -1,7 +1,15 @@
-import { useQuery } from '@apollo/client'
-import { ALL_AUTHORS } from '../queries'
+import { useMutation, useQuery } from '@apollo/client'
+import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries'
+import { useState } from 'react'
 
 const Authors = (props) => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  })
+
   const result = useQuery(ALL_AUTHORS)
   if (!props.show) {
     return null
@@ -9,27 +17,59 @@ const Authors = (props) => {
   if (result.loading) {
     return <div>loading...</div>
   }
+
+  const handleSetBirthyear = async (event) => {
+    event.preventDefault()
+    editAuthor({
+      variables: { name, setBornTo: parseInt(born) },
+    })
+    setName('')
+    setBorn('')
+  }
+
   const authors = result.data.allAuthors || []
 
   return (
     <div>
-      <h2>authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>born</th>
-            <th>books</th>
-          </tr>
-          {authors.map((a) => (
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
+      <div>
+        <h2>authors</h2>
+        <table>
+          <tbody>
+            <tr>
+              <th></th>
+              <th>born</th>
+              <th>books</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+            {authors.map((a) => (
+              <tr key={a.name}>
+                <td>{a.name}</td>
+                <td>{a.born}</td>
+                <td>{a.bookCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <h2>Set birthday</h2>
+      <form onSubmit={handleSetBirthyear}>
+        <div>
+          name{' '}
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          born{' '}
+          <input
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">update author</button>
+        </div>
+      </form>
     </div>
   )
 }
