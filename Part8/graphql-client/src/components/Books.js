ad9c1932eb50ab684c1ea87e2f7237fa6d39c571
unknown <JunkyardCat@users.noreply.github.com>
2023-07-27 20:45:59 +0800

    8.19

--- Part8/graphql-client/src/components/Books.js
+++ Part8/graphql-client/src/components/Books.js
@@ -1,8 +1,11 @@
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
+import { useState } from 'react'
 
 const Books = (props) => {
   const result = useQuery(ALL_BOOKS)
+  const [genre, setGenre] = useState('all')
+
   if (!props.show) {
     return null
   }
@@ -12,11 +15,15 @@ const Books = (props) => {
   }
 
   const books = result.data.allBooks || []
-
+  const temp = books.flatMap((book) => book.genres)
+  const genres = [...new Set(temp)]
+  //const genres = [...new Set(books.flatMap((book) => book.genres))]
+  //const temp = [...books.map((a) => a.genres)]
+  console.log('genre value', genre)
   return (
     <div>
       <h2>books</h2>
-
+      in genre <strong>{genre}</strong>
       <table>
         <tbody>
           <tr>
@@ -24,15 +31,25 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books
+            .filter((a) => (genre !== 'all' ? a.genres.includes(genre) : a))
+            .map((a) => (
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
+      <div>
+        {genres.map((genre) => (
+          <button key={genre} onClick={() => setGenre(genre)}>
+            {genre}
+          </button>
+        ))}
+        <button onClick={() => setGenre('all')}>all genre</button>
+      </div>
     </div>
   )
 }
