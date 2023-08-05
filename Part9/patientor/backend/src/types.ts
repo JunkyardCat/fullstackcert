edd7a759087bfb6fc5f175497ab82f0305b32175
unknown <JunkyardCat@users.noreply.github.com>
2023-08-05 22:34:34 +0800

    9.22

--- Part9/patientor/backend/src/types.ts
+++ Part9/patientor/backend/src/types.ts
@@ -9,7 +9,44 @@ export interface DiagnosesEntry {
   latin?: string;
 }
 
-export interface Entry {}
+export interface BaseEntry {
+  id: string
+  description: string
+  date: string
+  specialist: string
+  diagnosisCodes?: Array<DiagnosesEntry['code']>
+}
+
+export enum healthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck'
+  healthCheckRating: number
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: 'Hospital'
+  discharge: {
+    date: string
+    criteria: string
+  }
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare'
+  employerName: string
+  sickLeave?: {
+    startDate: string
+    endDate: string
+  }
+}
+
+export type Entry = HealthCheckEntry | HospitalEntry | OccupationalHealthcareEntry
 
 export interface PatientsEntry {
   id: string;
